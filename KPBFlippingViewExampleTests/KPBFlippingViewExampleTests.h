9a12b194265d4bdd9c2c0eb5b66ac2b942cffd59
Karl Bode <kpbode@gmail.com>
2013-05-06 18:29:46 +0200

    Initial Commit

---
+++ KPBFlippingViewExampleTests/KPBFlippingViewExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  KPBFlippingViewExampleTests.h
+//  KPBFlippingViewExampleTests
+//
+//  Created by Karl on 5/6/13.
+//  Copyright (c) 2013 Karl Bode. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KPBFlippingViewExampleTests : SenTestCase
+
+@end
---
+++ KPBFlippingViewExampleTests/KPBFlippingViewExampleTests.m
@@ -0,0 +1,32 @@
+//
+//  KPBFlippingViewExampleTests.m
+//  KPBFlippingViewExampleTests
+//
+//  Created by Karl on 5/6/13.
+//  Copyright (c) 2013 Karl Bode. All rights reserved.
+//
+
+#import "KPBFlippingViewExampleTests.h"
+
+@implementation KPBFlippingViewExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KPBFlippingViewExampleTests");
+}
+
+@end
